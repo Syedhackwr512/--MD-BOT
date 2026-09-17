442e3e908347b8f75ea90a5025bc16f9bfb0167c
Syedhackwr512 <watsappban92@gmail.com>
2026-09-17 23:12:12 +0500

    Add files via upload

---
+++ fakevCard.js
@@ -0,0 +1,15 @@
+module.exports = {
+    fakevCard: {
+        key: {
+            fromMe: false,
+            participant: "0@s.whatsapp.net",
+            remoteJid: "status@broadcast"
+        },
+        message: {
+            contactMessage: {
+                displayName: "© 𝐒𝐘𝐄𝐃 𝐇𝐀𝐂𝐊𝐄𝐑-",
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Meta\nORG:KIRA AI;\nTEL;type=CELL;type=VOICE;waid=13135550002:+13135550002\nEND:VCARD`
+            }
+        }
+    }
+};
